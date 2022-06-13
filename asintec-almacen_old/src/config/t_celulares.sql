29d743b69a7781641cbcdf2bcef096694c6f26ca
Adsan122 <adsanchez122@misena.edu.co>
2022-06-13 17:46:17 -0500

    docs

---
+++ asintec-almacen_old/src/config/t_celulares.sql
@@ -0,0 +1,8 @@
+create table celulares(
+    id int auto_increment primary key,
+    marca varchar(50) not null,
+    ref varchar(50) #referencia como marca samsung y referencia A30
+    so varchar(50) #sistema operativo
+    
+
+);
